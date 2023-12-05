7cb8d4e6d826d04d206727cfd71541f217908007
Ashraf-2 <hasanashraful03@gmail.com>
2023-12-05 20:19:33 +0600

    added popular menu item

---
+++ src/Pages/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../../Components/SectionTitle/SectionTitle";
+import MenuItem from "../Shared/MenuItem/MenuItem";
+
+const PopularMenu = () => {
+    const [menu,setMenu] = useState([]);
+    useEffect(()=> {
+        fetch('menu.json')
+            .then(res => res.json())
+            .then(data => {
+                const populerItems = data.filter(item => item.category ==="popular")
+                setMenu(populerItems)
+                // console.log(data);
+            })
+    }, [])
+    return (
+        <section className="my-6">
+            <SectionTitle
+                heading={"From Our Menu"}
+                subHeading={"From our items"}>
+            </SectionTitle>
+            {/* <h2>hello</h2> */}
+            <div className="grid md:grid-cols-2 gap-10">
+                {
+                    menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
+                }
+            </div>
+        </section>
+    );
+};
+
+export default PopularMenu;
