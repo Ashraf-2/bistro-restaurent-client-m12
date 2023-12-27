9b7476b73546cd1a73c153853d2ca22eb39fb8d5
Ashraf-2 <hasanashraful03@gmail.com>
2023-12-27 23:14:39 +0600

    added stripe payment method

--- src/Pages/Dashboard/Payment/CheckOutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckOutForm.jsx
@@ -1,8 +1,34 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import useCart from "../../../Hooks/useCart";
+import useAuth from "../../../Hooks/useAuth";
 
 const CheckOutForm = () => {
     const stripe = useStripe();
     const elements = useElements();
+    const {user} = useAuth();
+
+    const [error,setError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
+    const [transectionId, setTransectionId] = useState('');
+
+    const [cart] = useCart();
+    const TotalPrice = cart.reduce((total, item) => total + item.price, 0);
+
+    const axiosSecure = useAxiosSecure();
+    //fetch / load data from the backend server.
+    useEffect(()=> {
+        axiosSecure.post('/create-payment-intent', {price: TotalPrice})
+        .then(res => {
+            console.log(res.data.clientSecret)
+            setClientSecret(res.data.clientSecret)
+        })
+        .catch(error => {
+            console.log(error) 
+        })
+    }, [axiosSecure, TotalPrice])
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -22,8 +48,33 @@ const CheckOutForm = () => {
         });
         if (error) {
             console.log('[error]', error);
+            setError(error.message);
         } else {
             console.log('[PaymentMethod]', paymentMethod);
+            setError('')
+            setSuccess(`Your payment successfull and payment id: "${paymentMethod.id}"`)
+        }
+
+        //confirm payment
+        const {paymentIntent, error: confirmError} = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    email: user?.email || 'unknown',
+                    name: user?.displayName || 'unknown',
+
+                }
+            }
+        })
+        if(confirmError){
+            console.log("confirm Error",confirmError);  
+        }
+        else{
+            console.log("payment Intent: ",paymentIntent)
+            if(paymentIntent.status === "succeeded"){
+                console.log('payment successfull and the transection id : ', paymentIntent.id)
+                setTransectionId(paymentIntent.id)
+            }
         }
     }
     return (
@@ -45,9 +96,19 @@ const CheckOutForm = () => {
                         },
                     }}>
                 </CardElement>
-                <button className="btn btn-primary my-5" type="submit" disabled={!stripe}>
+                <button className="btn btn-primary my-5" type="submit" disabled={!stripe || !clientSecret}>
                     Pay
                 </button>
+                {
+                    error?<p>
+                        {error}
+                    </p>:
+                    <p>{success}</p>
+
+                }
+                {
+                    transectionId && <p className="text-center text-xl my-5 font-semibold">Your transection Id: <span className="italic text-red-500">  {transectionId}</span></p>
+                }
             </form>
         </div>
     );
--- src/Pages/Dashboard/Payment/payment_help_text.js
+++ src/Pages/Dashboard/Payment/payment_help_text.js
@@ -3,5 +3,9 @@
  * 2. create card element
  * 3. create stripe account and get publishable key
  * 4. use publishable key and use stripe to get card information and error.
- * 5.
+ * 5. create payment intent post endpoint api on server. make sure you already install stripe on the server side and get client secret. make sure you used the payment method types: ['card']
+ * 6. from client side get the client secret and save it.
+ * 7. use confirm card payment and pass user informaiton, card and client secret.
+ * 8. display transection id.
+ *  
  */
